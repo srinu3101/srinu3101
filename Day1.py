9551340a2f5d588c19afa59fdf0128a466e4d661
srinu3101 <163257818+srinu3101@users.noreply.github.com>
2024-08-18 14:05:31 +0530

    python basics

---
+++ Day1.py
@@ -0,0 +1,11 @@
+Covid_cases={
+    "Andhra":6000,
+    "chennai":8000,
+    "karnathaka":5000,
+    "telangana":7000,
+    "goa":4000,
+}
+state_names=Covid_cases.keys()
+print("state_names:",list(state_names))
+Covid_cases["india"]=1000,
+print("updated dictionary:", Covid_cases)
